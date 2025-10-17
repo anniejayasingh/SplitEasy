22402c614e81086bc3ca43074ae6c146ab7af385
Annie Jayasingh <anniejayasingh@gmail.com>
2025-10-17 15:27:17 +0400

    Initial commit - Spring Boot setup for SplitEasy

---
+++ src/test/java/com/splitwise/expense_sharing/ExpenseSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwise.expense_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
